cf6b4a3b7465b521732a11b48349b21f91c60ee3
C0A21162 <c0a2116262@edu.teu.ac.jp>
2022-11-22 16:10:26 +0900

    クイズ完成

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -9,14 +9,14 @@ def shutudai(qa_lst):
     return qa["a"]
 
 def kaitou(ans_lst):
-    st = datetime.datetime.now
+    st = datetime.datetime.now()
     ans = input("答えるんだ：")
-    ed = datetime.datetime.now
+    ed = datetime.datetime.now()
     if ans in ans_lst:
         print("正解!!!")
     else:
         print("出直してこい")
-    #print(f"回答時間：{(ed-st).seconds}秒" )
+    print(f"回答時間：{(ed-st).seconds}秒")
 
 if __name__ ==  "__main__":
     qa_lst = [
