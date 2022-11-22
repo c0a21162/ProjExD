9f20e935b407b9fa7c0ef2553cbfea00b83314f8
C0A21162 <c0a2116262@edu.teu.ac.jp>
2022-11-22 18:17:50 +0900

    アルファベット

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -1,27 +1,53 @@
 import random
 import datetime
 
-def shutudai(qa_lst):
-    qa = random.choice(qa_lst)
-    print("問題：" + qa["q"])
-    return qa["a"]
+num_of_alphabet = 26 #全アルファベット数
+num_of_all_chars = 10 #対象文字数
+num_of_abs_chars = 2 #欠損文字数
+num_of_trials = 2
 
-def kaitou(ans_lst):
-    st = datetime.datetime.now()
-    ans = input("答えるんだ：")
-    ed = datetime.datetime.now()
-    if ans in ans_lst:
-        print("正解!!!")
+def shutudai(alphabet):
+    all_chars = random.sample(alphabet,num_of_all_chars)
+    print("対象文字：")
+    for c in all_chars:
+        print(c , end = "")
+    print()
+
+    abs_chars = random.sample(alphabet,num_of_abs_chars)
+    print("欠損文字(デバック用)：") 
+    for c in abs_chars:
+        print(c,end="")
+    print()
+    return abs_chars
+
+
+def kaitou(abs_chars):
+    num = int(input("欠損文字はいくつあるでしょうか？"))
+    if num != num_of_abs_chars:
+        print("不正解です")
     else:
-        print("出直してこい")
-    print(f"回答時間：{(ed-st).seconds}秒")
+        print("正解です。それでは、具体的に欠損文字を一つずつ入力してください：")
+        for i in range(num):
+            ans = input(f"{i + 1}つ目の文字を入力してください：")
+            if ans not in abs_chars:
+                print("不正解です。")
+                return false
+            else:
+                abs_chars.remove(ans)
+        print("全部正解です。")
+        return True
+                
 
 if __name__ ==  "__main__":
-    qa_lst = [
-        {"q":"サザエの旦那の名前は？","a":["マスオ", "ますお"]},
-        {"q":"カツオの妹の名前は？","a":["ワカメ", "わかめ"]},
-        {"q":"タラオはカツオから見てどんな関係？","a":["甥","おい","甥っ子","おいっこ"]}
-    ]
-    ans_lst = shutudai(qa_lst)
-    kaitou(ans_lst)
-
+    st = datetime.datetime.now()
+    alphabet = [chr(i + 65) for i in range(num_of_alphabet)]
+    print(alphabet)
+    for i in range(num_of_trials):
+        abs_chars = shutudai(alphabet)
+        ret = kaitou(abs_chars)
+        if ret :
+            break
+        else:
+            print("-"*20)
+    ed = datetime.datetime.now()
+    print(f"所要時間：{(ed-st).seconds}秒")
