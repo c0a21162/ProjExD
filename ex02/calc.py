7937ea9422842fe825f26778f8c835da1d168d5e
C0A21162 <c0a2116262@edu.teu.ac.jp>
2022-11-29 14:23:36 +0900

    練習2-3

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,53 +1,28 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-
-def C(event):
-    if num == "C":
-        entry.delete(0, tk.END)
-
+#練習3
 def button_click(event):
-    btn=event.widget
-    num=btn["text"]
-    if num == "=":
-        eqn = entry.get()
-        res = eval(eqn)
-        entry.delete(0, tk.END)
-        entry.insert(tk.END,res)
-
-    else:
-        #tkm.showinfo("",f"{num}のボタンが押されました")
-         entry.insert(tk.END,num)
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    root.geometry("300x500")
-    root.title("電卓")
-
-    entry= tk.Entry(root,justify="right",width=10,font=("Times New Roman",40))
-    entry.grid(row=0,column=0,columnspan=3)
+    btn = event.widget
+    num = btn["text"]
+    tkm.showinfo("",f"{num}ボタンがクリックされました")
 
 
-    r, c= 1, 0
-    for i,num in enumerate([9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "+", "="]):
-        btn = tk.Button(root,
-                        text=f"{num}",
-                        width=4, 
-                        height=2,
-                        bg="#00ffff",
-                        font=("Times New Roman", 30)
-                        
-                    )
-        btn.bind("<1>",button_click)
-        btn.grid(row=r, column=c)
+#練習1
+root = tk.Tk()
+root.geometry("300x500")
+root.title("電卓")
 
-        c += 1
-        if (i+1)%3 == 0:
-            r += 1
-            c = 0
+#練習2
+r,c = 0,0
+for num in range(9,-1,-1):
+    button = tk.Button(root, text=f"{num}",width=4,height=2,font=("",30))
+    button.grid(row = r,column =c)
+    button.bind("<1>",button_click)
+    c += 1
+    if c%3 == 0:
+        r += 1
+        c =0
 
-    button_clear=tk.Button(root,text="C",height=2,width=4,font=("Times New Roman",30))
-    button_clear.grid(row=1,column=2)
-    button_clear.bind("<1>",C)
-    root.mainloop()
+root.mainloop()
     
